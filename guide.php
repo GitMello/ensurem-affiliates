2e42951b557c817ae1bccd522c2312341045128f
Leonard Mello <leonard@mellobrands.com>
2017-02-15 23:57:14 -0500

    couple minor mobile fixes.

--- guide.php
+++ guide.php
@@ -3,7 +3,7 @@
 
   <header id="guide-hero" class="l-fullpage-header flex flex-center just-center text-center flex-wrap text-shade link-no-shad link-hover-dark">
     <div class="l-fullpage-header-content no-pad-sides butt-link-light-lg">
-      <h4>Life Insurance &amp; Living Insurance Plans Tailored to YOUR Needs</h4>
+      <h2>Life Insurance &amp; Living Insurance <span class="smart-break">Plans Tailored to YOUR Needs</span></h2>
       <hr>
       <h1>Your Insurance Marketplace</h1>
       <a href="#">SHOW ME!</a>
@@ -13,15 +13,16 @@
   <article class="discover-content black text-center text-white">
     <div class="container-900">
       <h1>Life Insurance</h1>
-      <h3>Insurance coverage options for every stage of life</h3>
+      <h3>Insurance coverage options <span class="smart-break">for every stage of life</span></h3>
       <p>Financial Security. Peace of Mind. A Brighter Future.</p>
       <div class="text-left">
         <p>If you don’t have life insurance or you’re worried about your financial future, it’s not too late to pick out plans that will give you some peace of mind.</p>
         <p>Getting married? Now is the perfect time to buy Term Life Insurance for your spouse. Having children? Protect their financial future by buying a Whole Life Insurance policy.</p>
         <p>You can also shop our selection of Universal Life Insurance plans, Final Expense Insurance, and Accidental Death & Dismemberment Plans. We’ve streamlined the process of buying insurance online and you may not even have to talk to an agent to get what you need.</p>
       </div>
-      <h3>Discover Life Insurance Options Below</h3>
+      <h3>Discover Life Insurance <span class="smart-break">Options Below</span></h3>
       <hr>
+      <h2>"Buying Insurance Just Got <span class="smart-break">A Whole Lot Easier."</span></h2>
       <h1 class="brand-light">AffCo.</h1>
     </div>
   </article>
@@ -65,7 +66,7 @@
   <article class="discover-content dark text-center text-white">
     <div class="container-900">
       <h1>Living Insurance <sup>&trade;</sup></h1>
-      <h3>Live life to the fullest without financial stress</h3>
+      <h3>Live life to the fullest <span class="smart-break">without financial stress</span></h3>
       <div class="text-left">
         <p>High health insurance deductibles and a rising cost of living make it difficult to make ends meet. If you’re dealing with financial stress, protect your financial future now with Living Insurance plans — coverage and protection you need to live a better quality of life.</p>
         <p>Even if you have health insurance coverage, under Obamacare or otherwise, you could end up paying thousands of dollars for a high deductible and hospital fees if you were injured or diagnosed with a serious medical condition.</p>
@@ -73,10 +74,10 @@
         <p>Living Insurance products like Savings Plans give you discounts on everything from groceries to dental visits. Reduce financial stress without paying a premium price for the things you and your family need. Other Living Insurance plans include Dental Insurance, Disability Income Insurance, and Annuities.</p>
       </div>
       <p>Be financially prepared when life gets unpredictable.</p>
-      <h3>Discover Living Insurance Options Below</h3>
+      <h3>Discover Living Insurance <span class="smart-break">Options Below</span></h3>
       <hr>
       <h5><sup>1</sup> Kaiser Family Foundation</h5>
-      <h2>"Buying Insurance Just Got A Whole Lot Easier."</h2>
+      <h2>"Buying Insurance Just Got <span class="smart-break">A Whole Lot Easier."</span></h2>
       <h1 class="brand-light">AffCo.</h1>
     </div>
   </article>
