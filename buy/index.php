246ed129d27f3a2a3b30340d92c5e42115361f42
Leonard Mello <leonard@mellobrands.com>
2017-03-06 19:04:20 -0500

    working out product boxes.

--- buy/index.php
+++ buy/index.php
@@ -1,9 +1,11 @@
 <?php $title="Buy Life &amp; Living Insurance Plans"; ?>
 <?php include('../includes/topnav.php'); ?>
 
-  <header class="alt">
-    <div class="container no-pad">
-      <h1>Shop Life &amp; Living Insurance Plans</h1>
+  <header id="shop-hero" class="l-fullpage-header flex flex-center just-center text-center flex-wrap text-shade link-no-shad link-hover-dark">
+    <div class="l-fullpage-header-content xl no-pad-sides butt-link-light-lg">
+      <h2>Shop Life &amp; Living <span class="smart-break">Insurance Plans</span></h2>
+      <hr>
+      <h1>Protection &amp; More</h1>
     </div>
   </header>
 
--- includes/team-modals.php
+++ includes/team-modals.php
@@ -1,7 +1,7 @@
 <div id="modal-team-1" class="modal white butt-link">
   <a href="#<?php echo $team1;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-1.jpg" alt="<?php echo $team1;?>">
+    <img src="<?php echo $team1pic;?>" alt="<?php echo $team1;?>">
     <h2><?php echo $team1;?></h2>
     <a href="<?php echo $team1linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team1email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team1bio;?>
@@ -12,7 +12,7 @@
 <div id="modal-team-2" class="modal white butt-link">
   <a href="#<?php echo $team2;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-2.jpg" alt="<?php echo $team2;?>">
+    <img src="<?php echo $team2pic;?>" alt="<?php echo $team2;?>">
     <h2><?php echo $team2;?></h2>
     <a href="<?php echo $team2linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team2email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team2bio;?>
@@ -23,7 +23,7 @@
 <div id="modal-team-3" class="modal white butt-link">
   <a href="#<?php echo $team3;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-3.jpg" alt="<?php echo $team3;?>">
+    <img src="<?php echo $team3pic;?>" alt="<?php echo $team3;?>">
     <h2><?php echo $team3;?></h2>
     <!--<a href="<?php echo $team3linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> --><a href="mailto:<?php echo $team3email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team3bio;?>
@@ -34,7 +34,7 @@
 <div id="modal-team-4" class="modal white butt-link">
   <a href="#<?php echo $team4;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-4.jpg" alt="<?php echo $team4;?>">
+    <img src="<?php echo $team4pic;?>" alt="<?php echo $team4;?>">
     <h2><?php echo $team4;?></h2>
     <a href="<?php echo $team4linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team4email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team4bio;?>
@@ -45,7 +45,7 @@
 <div id="modal-team-5" class="modal white butt-link">
   <a href="#<?php echo $team5;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-5.jpg" alt="<?php echo $team5;?>">
+    <img src="<?php echo $team5pic;?>" alt="<?php echo $team5;?>">
     <h2><?php echo $team5;?></h2>
     <a href="<?php echo $team5linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team5email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team5bio;?>
@@ -56,7 +56,7 @@
 <div id="modal-team-6" class="modal white butt-link">
   <a href="#<?php echo $team6;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-6.jpg" alt="<?php echo $team6;?>">
+    <img src="<?php echo $team6pic;?>" alt="<?php echo $team6;?>">
     <h2><?php echo $team6;?></h2>
     <a href="<?php echo $team6linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team6email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team6bio;?>
@@ -67,7 +67,7 @@
 <div id="modal-team-7" class="modal white butt-link">
   <a href="#<?php echo $team7;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-7.jpg" alt="<?php echo $team7;?>">
+    <img src="<?php echo $team7pic;?>" alt="<?php echo $team7;?>">
     <h2><?php echo $team7;?></h2>
     <a href="<?php echo $team7linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team7email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team7bio;?>
@@ -78,7 +78,7 @@
 <div id="modal-team-8" class="modal white butt-link">
   <a href="#<?php echo $team8;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-8.jpg" alt="<?php echo $team8;?>">
+    <img src="<?php echo $team8pic;?>" alt="<?php echo $team8;?>">
     <h2><?php echo $team8;?></h2>
     <a href="<?php echo $team8linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team8email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team8bio;?>
@@ -89,7 +89,7 @@
 <div id="modal-team-9" class="modal white butt-link">
   <a href="#<?php echo $team9;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-9.jpg" alt="<?php echo $team9;?>">
+    <img src="<?php echo $team9pic;?>" alt="<?php echo $team9;?>">
     <h2><?php echo $team9;?></h2>
     <a href="<?php echo $team9linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team9email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team9bio;?>
@@ -100,7 +100,7 @@
 <div id="modal-team-10" class="modal white butt-link">
   <a href="#<?php echo $team10;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-10.jpg" alt="<?php echo $team10;?>">
+    <img src="<?php echo $team10pic;?>" alt="<?php echo $team10;?>">
     <h2><?php echo $team10;?></h2>
     <a href="<?php echo $team10linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team10email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team10bio;?>
@@ -111,7 +111,7 @@
 <div id="modal-team-11" class="modal white butt-link">
   <a href="#<?php echo $team11;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-11.jpg" alt="<?php echo $team11;?>">
+    <img src="<?php echo $team11pic;?>" alt="<?php echo $team11;?>">
     <h2><?php echo $team11;?></h2>
     <a href="<?php echo $team11linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team11email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team11bio;?>
@@ -122,7 +122,7 @@
 <div id="modal-team-12" class="modal white butt-link">
   <a href="#<?php echo $team12;?>" class="close"><i class="fa fa-close"></i></a>
   <div class="container-900 relative">
-    <img src="/assets/team/team-12.jpg" alt="<?php echo $team12;?>">
+    <img src="<?php echo $team12pic;?>" alt="<?php echo $team12;?>">
     <h2><?php echo $team12;?></h2>
     <a href="<?php echo $team12linkedin;?>" target="_blank"><i class="fa fa-linkedin"></i></a> <a href="mailto:<?php echo $team12email;?>"><i class="fa fa-envelope"></i></a>
     <?php echo $team12bio;?>
