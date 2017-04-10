53e652d52561778c405a6dcee10adfa8a357351d
Leonard Mello <leonard@mellobrands.com>
2017-04-10 17:01:11 -0400

    styling blog page

--- blog/index.php
+++ blog/index.php
@@ -1,18 +1,347 @@
 <?php $title="Awaring America | Ensurem Blog"; ?>
 <?php include('../includes/topnav.php'); ?>
 
-<header id="blog-hero" class="l-fullpage-header flex flex-center just-center text-center flex-wrap text-shade link-no-shad link-hover-dark">
-  <div class="l-fullpage-header-content xl no-pad-sides butt-link-light-lg">
-    <h1><i class="fa fa-newspaper-o"></i> <span class="text-accent">|</span> Awaring America<sup>&trade;</sup></h1>
-    <hr>
-    <h2>Articles, News &amp; More</h2>
-    <a href="#">#Guides</a>
-    <a href="#">#Life</a>
-    <a href="#">#Money</a>
-    <a href="#">#Insurance</a>
-    <a href="#">#Fun</a>
-    <a href="#">#Parents</a>
-  </div>
-</header>
+  <header id="blog-hero" class="l-fullpage-header flex flex-center just-center text-center flex-wrap text-shade link-no-shad link-hover-dark">
+    <div class="l-fullpage-header-content xl no-pad-sides butt-link-light-lg">
+      <h1><i class="fa fa-newspaper-o"></i> <span class="text-accent">|</span> Awaring America<sup>&trade;</sup></h1>
+      <hr>
+      <h2>Articles, News &amp; More</h2>
+      <a href="#">#Life Insurance</a>
+      <a href="#">#Living Insurance</a>
+      <a href="#">#Health</a>
+      <a href="#">#Wealth</a>
+      <a href="#">#Life</a>
+      <form class="flex flex-wrap just-center">
+        <label class="flex relative">
+          <span class="text-no-shad">Search</span>
+          <input  placeholder="What are you looking for?" type="search" tabindex="1" required="required">
+        </label>
+        <button name="submit" type="submit" id="contact"><i class="fa fa-search"></i> | Search</button>
+      </form>
+    </div>
+  </header>
+
+  <article class="blog l-inline-all-ul dark">
+    <div class="container">
+
+      <ul class="flex flex-stretch flex-wrap butt-link-primary">
+        <li class="col-pad flex-grow-1 col-md-6 col-lg-6 butt-link-light hr-white hr-left">
+          <h2>Latest Life Insurance</h2>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <h3>Subcategories</h3>
+          <a href="#">#wealth</a> <a href="#">#life-insurance</a> <a href="#">#living-insurance</a>
+          <form class="flex flex-wrap just-center" style="margin-top:2em">
+            <label class="flex relative">
+              <span class="text-no-shad">Search All Blogs</span>
+              <input  placeholder="Start typing..." type="search" tabindex="1" required="required">
+            </label>
+            <button name="submit" type="submit" id="contact"><i class="fa fa-search"></i> | Search</button>
+          </form>
+        </li>
+
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+
+          <header class="mid relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+            <div class="cat flex flex-wrap">
+              <h6 class="blog-wealth annuco">#Wealth</h6>
+              <h6 class="blog-wealth petco">#Life-Insurance</h6>
+              <h6 class="blog-wealth homeseco">#Living-Insurance</h6>
+            </div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+
+          <header class="mid relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+            <div class="cat flex flex-wrap">
+              <h6 class="blog-wealth annuco">#Wealth</h6>
+              <h6 class="blog-wealth petco">#Life-Insurance</h6>
+              <h6 class="blog-wealth homeseco">#Living-Insurance</h6>
+            </div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+
+          <header class="mid relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+            <div class="cat flex flex-wrap">
+              <h6 class="blog-wealth annuco">#Wealth</h6>
+              <h6 class="blog-wealth petco">#Life-Insurance</h6>
+              <h6 class="blog-wealth homeseco">#Living-Insurance</h6>
+            </div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+
+          <header class="mid relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+            <div class="cat flex flex-wrap">
+              <h6 class="blog-wealth annuco">#Wealth</h6>
+              <h6 class="blog-wealth petco">#Life-Insurance</h6>
+              <h6 class="blog-wealth homeseco">#Living-Insurance</h6>
+            </div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <!--<li class="flex-grow-1 col-md-6 col-lg-4">
+          <div class="flex">
+            <h6 class="blog-wealth termco">#Wealth</h6>
+            <h6 class="blog-wealth finco">#Life-Insurance</h6>
+          </div>
+          <header class="relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+          <header class="relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <hr>
+            <div class="text-center">
+            <a href="#">Read Now</a>
+            </div>
+            <div class="flex just-center flex-wrap">
+              <h6 class="blog-wealth termco">#Wealth</h6>
+              <h6 class="blog-wealth finco">#Life-Insurance</h6>
+            </div>
+          </article>
+
+        </li> -->
+
+
+      </ul>
+      <form class="flex flex-wrap just-center">
+        <label class="flex relative">
+          <span class="text-no-shad">Search All Blogs</span>
+          <input  placeholder="What are you looking for?" type="search" tabindex="1" required="required">
+        </label>
+        <button name="submit" type="submit" id="contact"><i class="fa fa-search"></i> | Search</button>
+      </form>
+
+  </article>
+
+    <article class="blog l-inline-all-ul butt-link-primary termco">
+      <div class="container">
+        <h2>Latest News</h2>
+        <h3>Category</h3>
+      <h3>Category</h3>
+
+      <ul class="flex flex-stretch flex-wrap">
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+
+          <header class="relative hr-white" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+            <div class="cat flex flex-wrap">
+              <h6 class="blog-wealth">#Wealth</h6>
+              <h6 class="blog-wealth">#Life-Insurance</h6>
+              <h6 class="blog-wealth">#Living-Insurance</h6>
+            </div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+
+          <header class="mid relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+            <div class="cat flex flex-wrap">
+              <h6 class="blog-wealth annuco">#Wealth</h6>
+              <h6 class="blog-wealth petco">#Life-Insurance</h6>
+              <h6 class="blog-wealth homeseco">#Living-Insurance</h6>
+            </div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+          <header class="relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+          </header>
+          <article class="col-pad relative">
+            <div class="flex just-center flex-wrap">
+              <h6 class="blog-wealth primary">#Wealth</h6>
+              <h6 class="blog-wealth accent">#Life-Insurance</h6>
+            </div>
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <!--<li class="flex-grow-1 col-md-6 col-lg-4">
+          <div class="flex">
+            <h6 class="blog-wealth termco">#Wealth</h6>
+            <h6 class="blog-wealth finco">#Life-Insurance</h6>
+          </div>
+          <header class="relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <div class="text-right">
+            <a href="#">Read Now</a>
+            </div>
+          </article>
+        </li>
+
+        <li class="flex-grow-1 col-md-6 col-lg-4">
+          <header class="relative" id="blog-test">
+            <div class="abso-centered">
+
+            <h3>Risky Business</h3>
+            <hr>
+            <h4>Life Insurance Advice for Adventurers</h4></div>
+          </header>
+          <article class="col-pad">
+            <div class="">
+            <p>You’re a risk taker.
+            You love the thrill of outdoor adventures.
+            You know the ones – the ones that get your blood pumping and your adrenaline flowing. Extreme sports and adrenaline-fueled activities of any (and ALL!) kinds, they define you.</p>
+            </div>
+            <hr>
+            <div class="text-center">
+            <a href="#">Read Now</a>
+            </div>
+            <div class="flex just-center flex-wrap">
+              <h6 class="blog-wealth termco">#Wealth</h6>
+              <h6 class="blog-wealth finco">#Life-Insurance</h6>
+            </div>
+          </article>
+
+        </li> -->
+
+
+      </ul>
+    </div>
+  </article>
 
 <?php include(includes.'footer.php'); ?>
